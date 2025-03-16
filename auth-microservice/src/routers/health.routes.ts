eaa59e7fa39db6fe2d443cdaed3958eb83ac97f7
SubhamJoshi18 <shubhamrajjoshi69@gmail.com>
2025-03-16 19:14:17 +0545

    complete the swagger setup for the auth

--- auth-microservice/src/routers/health.routes.ts
+++ auth-microservice/src/routers/health.routes.ts
@@ -8,6 +8,33 @@ import { verifyAuthAccessToken } from '../middleware/auth.middleware';
 
 const healthRouter = Router();
 
+/**
+ * @openapi
+ * /health:
+ *   get:
+ *     summary: Health Check
+ *     description: Returns the health status of the API.
+ *     tags:
+ *       - Health
+ *     responses:
+ *       202:
+ *         description: API is up and running.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "The Route : /health is Working!!"
+ *                 time:
+ *                   type: string
+ *                   format: date
+ *                   example: "Sun Mar 16 2025"
+ *                 error:
+ *                   type: boolean
+ *                   example: false
+ */
 healthRouter.get('/health', (req: Request, res: Response): void => {
   res.status(statusCode.ACCEPTED).json({
     message: `The Route : ${req.originalUrl} is Working!!`,
